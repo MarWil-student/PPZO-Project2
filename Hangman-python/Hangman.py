96e2b1a6b469723c29ce847a777302075821f492
MarWil-student <martyna.wilkosz@student.wsb.edu.pl>
2026-04-21 21:08:42 +0200

    Done with structure

--- Hangman-python/Hangman.py
+++ Hangman-python/Hangman.py
@@ -20,6 +20,49 @@ class Player:
     def register_letter(self, letter):
         selg.guessed_letters.add(letter)
 
+class Game:
+    def __init__(self):
+        self.__word_bank = WordBank()
+        self.__secret_word = self.__word_bank.get_random_word()
+        self.__player = Player()
+        self.__max_mistakes = 6
+    def start(self):
+        print("====  Gra Wisielec  ====")
+        print(f"Zasady: Zgadnij slowo, maksymalna ilosc pomylek to {self.__max_mistakes}")
+        while self.__player.mistakes < self.__max_mistakes:
+            self.__display_board()
+            guess = self.__get_valid_input()
+            if self.__player.already_guessed(guess):
+                print(f"\n Litera '{guess}' byla juz wybrana")
+                continue
+            self.__player.register_letter(guess)
+            if guess in self.__secret_word:
+                print("\n Poprawna litera!")
+            else:
+                self.__player.ad_mistake()
+                print(f"\n Pudlo! Pozostalo bledow: {self.__max_mistakes - self.__player.mistakes}")
+            if self.__win():
+                self.__display_board()
+                print("\n====  Gratulacje wygrana!  ====")
+                return
+         print(f"\nPrzegrana! Wisielec gotowy. Slowo to: {self.__secret_word}")
+    def __display_board(self):
+        print("\nSlowo:")
+        display = [c if self.__player.already_guessed(c) else "_" for c in self.__secret_word]
+        print(" ".join(display))
+    def __get_valid_input(self):
+        while True:
+            user_input = input("\nPodaj jedna litere: ").strip().lower()
+            if not user_input or len(user_input != 1:
+                print("BLAD: Musisz wpisac dokladnie jedna litere")
+                continue
+            if not user_input.isalpha():
+                print("BLAD: Znaki inne niz litery nie sa akceptowane")
+                continue
+            return user_input
+    def __win(self):
+        return all(c in self.__player.guessed_letters for c in self.__secret_word)
+
 if __name__ == "__main__":
     game = Game()
     game.start()
